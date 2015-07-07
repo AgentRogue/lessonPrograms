bba7a3cf8b1ff83c0bc429a41461f12343b94c51
TJ Challstrom <colonelrouge@gmail.com>
2015-07-06 23:56:13 -0700

    prog11 Update

--- src/prog11/Menu.java
+++ src/prog11/Menu.java
@@ -1,7 +1,12 @@
 package prog11;
 
+import progX.Food;
+
 /**
  * Created by TJ Challstrom on 29-Jun-15.
  */
 public interface Menu {
+    public void cook(Food food);
+
+    public int getTotal();
 }
--- src/prog11/MexicanKitchen.java
+++ src/prog11/MexicanKitchen.java
@@ -3,10 +3,10 @@ package prog11;
 import progX.Food;
 
 /**
- * Created by TJ Challstrom on 29-Jun-15.
+ * Created by TJ Challstrom on 06-Jul-15.
  *
  */
-public class Kitchen implements Menu {
+public class MexicanKitchen implements Menu {
     @Override
     public void cook(Food food) {
 
